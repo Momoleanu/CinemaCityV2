7be0b903ae5b31bd98d19064c7cf981ef71a69ad
Mihnea-Bogdan Holban <mihnea-bogdan.holban@student.tuiasi.ro>
2023-05-23 22:07:24 +0300

    Fix err

--- ProiectIP/Startup.cs
+++ ProiectIP/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ProiectIP.Data;
+using ProiectIP.Data.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,9 @@ namespace ProiectIP
             //Constructor sql
             services.AddDbContext<AppDbContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
             services.AddControllersWithViews();
+
+            //Configurare Servicii
+            services.AddScoped<IActorsService, ActorsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
